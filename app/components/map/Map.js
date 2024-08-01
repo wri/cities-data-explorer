46b0dd96e53b9ffec651d0e79ce3ce974a0a7b8f
Arsh <arsh3doda@gmail.com>
2024-08-01 17:52:38 +0530

    CEE-58 Implemented Airtable Layer Fetching

--- app/components/map/Map.js
+++ app/components/map/Map.js
@@ -3,12 +3,10 @@
 import { DeckGL } from "deck.gl";
 import {
     useContext,
-    useEffect,
     useRef
 } from "react";
 import { Map as RMap } from 'react-map-gl';
 import { MapContext } from "../../Base";
-
 
 const INITIAL_VIEW_STATE = {
     longitude: -122.41669,
@@ -21,8 +19,6 @@ function Map() {
     const ctx = useContext(MapContext);
     const setMap = ctx?.setMap;
 
-    useEffect(() => {
-    }, []);
 
     return (
         <DeckGL
@@ -31,7 +27,7 @@ function Map() {
         >
             <RMap
                 mapStyle="mapbox://styles/mapbox/light-v9"
-                mapboxAccessToken="pk.eyJ1IjoicmVzb3VyY2V3YXRjaCIsImEiOiJjbHo4dWU5NnMwNHpzMmlvanJtODBma2Y4In0.bXap405a9vR6BLcTpaTdZA"
+                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_KEY}
             />
         </DeckGL>
     )
--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Base from "./Base";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen bg-white">
+    <main className="flex h-screen bg-white">
       <Base />
     </main>
   );
